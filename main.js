161ad0a879b3466ebb30ac0c633255647f442e6a
Nasser Kessas <59720679+nasserkessas@users.noreply.github.com>
2023-02-14 08:18:26 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 const play = () => {
   link = document.getElementById("link").value;
-  document.getElementById("frame").innerHTML = `<iframe title="YouTube Video" src="https://www.youtube-nocookie.com/embed/${link.match(/(youtube.com%7Cyoutube-nocookie.com%7Cyoutu.be)\/(watch\?v=|v\/|embed\/)([0-9A-Za-z-_]{11})/)[3]}?rel=0&amp;autoplay=1" allowfullscreen=""></iframe>`
+  document.getElementById("frame").innerHTML = `<iframe title="YouTube Video" src="https://www.youtube-nocookie.com/embed/${link.match(/(youtube.com|youtube-nocookie.com|youtu.be)\/(watch\?v=|v\/|embed\/)([0-9A-Za-z-_]{11})/)[3]}?rel=0&amp;autoplay=1" allowfullscreen=""></iframe>`
 }
